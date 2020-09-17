6ab8b712effdc2c8a256c519439ec9ccb356d509
Alberto <alberto.izquierdomo@gmail.com>
2020-09-17 12:40:26 +0200

    Set console windows size added to comply with 80x32 dimensions

--- Games1-4.MarsAttacks/Games1-4.MarsAttacks.cpp
+++ Games1-4.MarsAttacks/Games1-4.MarsAttacks.cpp
@@ -1,7 +1,26 @@
 #include "curses.h"
 #include "StaticLib-Utils.h"
 
-int main()
+// define must be typed before windows.h; curses.h must be typed before both
+#define _WIN32_WINNT 0x0500 // Used to resize console window. It says that you are running this program on Windows 2000 or higher.
+#include <windows.h>
+
+#include <iostream>
+
+void SetConsoleWindowSize();
+
+int main(void)
 {
-	return 0;
+    //SetConsoleWindowSize();
+    return 0;
+}
+
+void SetConsoleWindowSize() {
+    HWND console = GetConsoleWindow();
+    RECT r;
+    GetWindowRect(console, &r); //stores the console's current dimensions
+    //int width{ r.right - r.left };
+    //int height{ r.bottom - r.top};
+    // MoveWindow(window_handle, x, y, width, height, redraw_window);
+    MoveWindow(console, r.left, r.top, 607, 550, TRUE); // Values get by setting console size manually when running the program
 }
